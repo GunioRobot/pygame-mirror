a2a23fe6af5303a63ca42eb57623c2893e86ec56
Werner Laurensse <el.lauwer@gmail.com>
2009-08-22 16:55:22 +0200

    test

--- config_darwin.py
+++ config_darwin.py
@@ -95,7 +95,7 @@ DEPS = [
     Dependency('JPEG', 'jpeglib.h', 'libjpeg', ['jpeg']),
     Dependency('SCRAP', '','',[]),
     Dependency('PORTMIDI', 'portmidi.h', 'libportmidi', ['portmidi']),
-    FrameworkDependency('PORTTIME', 'CoreMidi.h', 'CoreMidi', 'CoreMidi'),
+    FrameworkDependency('PORTMIDITIME', 'CoreMidi.h', 'CoreMidi', 'CoreMidi'),
     FrameworkDependency('COCOA', 'Cocoa.h', 'Cocoa', 'Cocoa'),
     FrameworkDependency('QUICKTIME', 'QuickTime.h', 'QuickTime', 'QuickTime'),t
 ]
